7249322e6489895d91cdc17b8cf8c794cd654590
Thong-dev <thong.nguyen1820031956@hcmut.edu.vn>
2025-03-22 13:11:23 +0700

    ADD: STATE header

---
+++ include/states.h
@@ -0,0 +1,18 @@
+#ifndef STATES_H
+#define STATES_H
+
+enum State {
+    CHUA_CO_KET_NOI,
+    KET_NOI_BLE,
+    CHO_THONG_TIN_WIFI,
+    KET_NOI_WIFI,
+    NHAN_THONG_TIN_WIFI,
+    KET_NOI_WIFI_THANH_CONG,
+    GUI_DU_LIEU_MQTT,
+    CHE_DO_BLE,
+    CHE_DO_LAN,
+    CHE_DO_AP, 
+    SEND_FLASH_DATA
+};
+
+#endif
